3d696bebf931f3b35cdc72112f2970f1ba168e71
olympiawoj <owojcik7@gmail.com>
2021-04-26 23:58:39 -0400

    useReducer

--- src/App.js
+++ src/App.js
@@ -1,31 +1,52 @@
-import React, { useState } from 'react';
+import React, { useReducer } from 'react';
 import Tabs from './components/Tabs';
 import './App.css';
 import AddressBar from './components/AddressBar';
 
-export default function App() {
-  const [browsers, setBrowsers] = useState([
-    'https://chrisoncode.io/',
-    'https://www.commonstock.com/'
-  ])
-  const [activeBrowser, setActiveBrowser] = useState(0)
-
-  function chooseBrowser(id) {
-    setActiveBrowser(id)
-  }
-
-  function addBrowser() {
+function reducer(state, action) {
+  const { browsers, activeBrowser } = state
+  const { type, payload } = action
+  if (type === 'ADD') {
     const newBrowsers = [...browsers, '']
-    setBrowsers(newBrowsers)
-    setActiveBrowser(newBrowsers.length-1)
+    const activeBrowser = browsers.length - 1;
 
+    return { browsers: newBrowsers, activeBrowser }
   }
 
-  function updateBrowser(url){
+  if (type === 'CHOOSE') {
+    return {
+      ...state,
+      activeBrowser: payload
+    }
+  }
+
+  if (type === 'UPDATE') {
     const newBrowsers = [...browsers]
-    newBrowsers[activeBrowser] = url
-    setBrowsers(newBrowsers)
+    newBrowsers[activeBrowser] = payload;
+    return {
+      ...state,
+      browsers: newBrowsers
+    }
   }
+  if (type === 'CLOSE') { }
+}
+
+
+export default function App() {
+  const [{ browsers, activeBrowser }, dispatch] = useReducer(reducer, {
+    browsers: [
+      'https://chrisoncode.io/',
+      'https://www.commonstock.com/'
+    ],
+    activeBrowser: 0
+  })
+
+  // formatting or grabbing of data
+  // const { browsers, activeBrowser } = state
+
+  const chooseBrowser = id => dispatch({ type: 'CHOOSE', payload: id })
+  const addBrowser = () => dispatch({ type: 'ADD' })
+  const updateBrowser = url => dispatch({ type: 'UPDATE', payload: url })
 
   const url = browsers[activeBrowser]
 
